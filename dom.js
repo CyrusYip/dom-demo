2cb4eee3eb23584387a62b3938053c91ea58afc4
Cyrus Yip <60951091+CyrusYip@users.noreply.github.com>
2021-10-22 20:53:43 +0800

    init

---
+++ dom.js
@@ -0,0 +1,36 @@
+window.dom = {
+  find(selector, scope) {
+    return (scope || document).querySelectorAll(selector);
+  },
+  style(node, name, value) {
+    if (arguments.length === 3) {
+      // (node, color ,'red')
+      node.style[name] = value;
+    } else if (arguments.length === 2) {
+      if (typeof name === "string") {
+        // (node, 'color')
+        return node.style[name];
+      } else if (name instanceof Object) {
+        //(node, {color: red; border: 1px solid red})
+        for (key in name) {
+          node.style[key] = name[key];
+        }
+      }
+    }
+  },
+  each(nodeList, fn) {
+    for (let i = 0; i < nodeList.length; i++) {
+      fn.call(null, nodeList[i]);
+    }
+  },
+};
+
+// <div id="test">
+//     <div className="red"></div>
+// </div>
+
+const div = dom.find("#test>.red")[0]; // 获取对应的元素
+dom.style(div, "color", "red"); // 设置 div.style.color
+
+const divList = dom.find(".red"); // 获取多个 div.red 元素
+dom.each(divList, (n) => console.log(n)); // 遍历 divList 里的所有元素
